2f103f73a7f9cb4cc69c9a075b31b07b3020d3cc
wwg <wwg@qq.com>
2022-05-23 13:06:06 +0800

    项目初始化

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <van-cell-group>
+      <van-cell title="单元格" value="内容"></van-cell>
+      <van-cell title="单元格" value="内容"></van-cell>
+    </van-cell-group>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+//引入全部vant组件
+//缺点：打包体积大，速度慢
+import Vant from 'vant';
+import 'vant/lib/index.css';
+Vue.use(Vant);//注册vant全局
 
 Vue.config.productionTip = false
 
